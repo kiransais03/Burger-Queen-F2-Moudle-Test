aad399ec006f3cce3f0f45fec5f9479f7ac02fe1
kiransai <kiransais03@gmail.com>
2023-06-04 23:15:04 +0530

    changes updated

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
+//JSON Data File URl
 let endpoint =`https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json`;
 
+
+//GetMenu Fucntion 
 async function getMenu(){
     try {
     let response = await fetch(endpoint);
@@ -32,14 +35,16 @@ async function getMenu(){
     }
 }
 
-// getMenu();
-
 
 {/* <div class="add-remove">
  <img src="" class="remove">
 <input type="text" style="width:20px;text-align:center;font-size:20px;" disabled value="0">
  <img src="" class="add">
 </div> */}
+
+
+
+//Take Order Function
 
 async function takeOrder(result) {
     try {
@@ -72,6 +77,7 @@ async function takeOrder(result) {
         rightcart.append(additems);
 
        resolve();
+       console.log("Take Order");
        console.log(orderpromise);
        return orderpromise;
     }
@@ -81,20 +87,29 @@ catch(error) {
 }
 }
 
+
+
+//Order Preparation Function
+
 async function orderPrep() {
     try{
    let prepareprom = new Promise ((resolve)=>{setTimeout(() => preparing(resolve),1500)});
 
-   function preparing(resolve) {
+   async function preparing(resolve) {
     let object = {order_status:true,paid:false};
     resolve(object);
 }
+console.log("Order Prep");
+console.log(prepareprom);
 return prepareprom;
 }
 catch(error) {
     console.log(error);
 }
 }
+
+
+//Payment Order Function
 
 async function payOrder(object) {
 try {
@@ -104,6 +119,8 @@ try {
         object={order_status:true,paid:true};
         resolve(object);
     }
+    console.log("Pay Order");
+    console.log(paymentprom);
     return paymentprom;
 }
 catch(error) {
@@ -112,6 +129,8 @@ catch(error) {
 }
 
 
+
+//ThankYou Function
 async function thankyouFnc(payment) {
     try{
     if(payment) {
@@ -125,6 +144,9 @@ catch(error) {
 }
 
 
+
+
+//Functions Exceution Process Step By Step
 async function pageload() {
     let itemdata=await getMenu();
 
@@ -140,217 +162,3 @@ async function pageload() {
 }
 
 pageload();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // getMenu() function
-// async function getMenu() {
-//     try {
-//         const response = await fetch('https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json');
-//         const menuData = await response.json();
-
-//         // Displaying food items to the user
-//         console.log('Menu:', menuData);
-
-//         // Get the container element
-//         const container = document.getElementById('menu-container');
-
-//         // Loop through the menu items
-//         menuData.forEach(item => {
-//             // Create a new div for each menu item
-//             const menuItem = document.createElement('div');
-//             menuItem.classList.add('menu-item');
-
-//             // Create an image element
-//             const image = document.createElement('img');
-//             image.src = item.imgSrc;
-//             image.alt = item.name;
-
-//             // Create a heading element for the menu item name
-//             const name = document.createElement('h3');
-//             name.textContent = item.name;
-
-//             // Create a paragraph element for the menu item price
-//             const price = document.createElement('p');
-//             price.textContent = 'Price: ' + item.price;
-
-//             // Append the image, name, and price elements to the menu item div
-//             menuItem.appendChild(image);
-//             menuItem.appendChild(name);
-//             menuItem.appendChild(price);
-
-//             // Append the menu item div to the container
-//             container.appendChild(menuItem);
-
-//         });
-//         return menuData;
-//     } catch (error) {
-//         console.error('Error:', error);
-//     }
-// }
-
-
-
-// function takeOrder(menu) {
-//     return new Promise(resolve => {
-//         setTimeout(() => {
-//             const ordersContainer = document.getElementById('orders-container');
-//             //const menuContainer = document.getElementById('menu-container');
-//             ordersContainer.innerHTML = ''; // Clear previous orders
-//             //menuContainer.innerHTML = ''; // Clear menu items
-
-
-//             const orders = [];
-//       for (let i = 0; i < 3; i++) {
-//         const randomIndex = Math.floor(Math.random() * menu.length);
-//         const order = menu[randomIndex];
-//         orders.push(order);
-
-//         const orderItem = document.createElement('div');
-//         orderItem.classList.add('order-item');
-
-//         const image = document.createElement('img');
-//         image.src = order.imgSrc;
-//         image.alt = order.name;
-
-//         const name = document.createElement('h3');
-//         name.textContent = order.name;
-
-//         const price = document.createElement('p');
-//         price.textContent = 'Price: ' + order.price;
-
-//         orderItem.appendChild(image);
-//         orderItem.appendChild(name);
-//         orderItem.appendChild(price);
-
-//         ordersContainer.appendChild(orderItem);
-//       }
-
-//             resolve(orders);
-//         }, 2500);
-//     });
-// }
-
-
-
-
-// // orderPrep() function
-// function orderPrep() {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             const orderStatus = {
-//                 order_status: true,
-//                 paid: false,
-//             };
-//             if (orderStatus.order_status && !orderStatus.paid) {
-//                 resolve(orderStatus);
-//             } else {
-//                 reject(new Error("Failed to prepare the order."));
-//             }
-//         }, 1500);
-//     });
-// }
-
-// // payOrder() function
-// function payOrder() {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             const orderStatus = {
-//                 order_status: true,
-//                 paid: true,
-//             };
-//             if (orderStatus.order_status && orderStatus.paid) {
-//                 resolve(orderStatus);
-//             } else {
-//                 reject(new Error("Failed to process the payment."));
-//             }
-//         }, 1000);
-//     });
-// }
-
-// // thankyouFnc() function
-// function thankyouFnc() {
-//     alert('Thank you for eating with us today!');
-// }
-
-// // Execute the restaurant flow when the screen loads
-// window.onload = async function () {
-//     const menuData = await getMenu();
-//     // console.log('Menu:', menuData);
-
-//     const order = await takeOrder(menuData);
-//     console.log('Order:', order);
-
-//     const orderStatus = await orderPrep();
-//     console.log('Order Status:', orderStatus);
-
-//     const paidOrder = await payOrder();
-//     console.log('Order Status:', paidOrder);
-
-//     thankyouFnc();
-// };
